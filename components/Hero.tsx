f52c3500e1978572a6893fca005f99d88b48d853
AleksaMarjanov <aleksa.marjanov@outlook.com>
2023-05-05 17:33:39 -0500

    change width of picture again..

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -8,13 +8,13 @@ import { fadeIn, staggerContainer, textVariant } from '@/utils/motion'
 
 const Hero = () => {
     return (
-        <section className="w-full relative flex justify-center sm:px-16 xl:px-36 min-h-screen bg-[#aec2bc]">
+        <section className="w-full relative flex justify-center sm:px-16 xl:px-36 max-[475px]:min-h-[80vh] min-h-screen bg-[#aec2bc]">
             {/* <div className="wave-hero"> */}
             {/*     <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none"> */}
             {/*         <path d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z" className="shape-fill"></path> </svg> */}
             {/* </div> */}
 
-            <div className="wave3 absolute z-[30]">
+            <div className="wave3 absolute z-[30] !max-[475px]:h-[150px]">
                 <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
                     <path d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z" className="shape-fill"></path>
                 </svg>
@@ -63,7 +63,7 @@ const Hero = () => {
                 <motion.div
                     variants={fadeIn('right', 'tween', 0.1, 0.6)}
                     className="relative z-[0] lg:order-1 
-                    max-[475px]:w-[280px] w-[420px] h-[420px] lg:w-[670px] xl:w-[820px]">
+                    max-[475px]:w-[320px] w-[420px] h-[420px] lg:w-[670px] xl:w-[820px]">
                     <Image
                         src="/heroImag2Ver.png"
                         alt="heroImage"
