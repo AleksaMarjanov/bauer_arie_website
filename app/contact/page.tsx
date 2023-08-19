840af1f2f7052ef5484b45ec42a380faeebdb72e
AleksaMarjanov <aleksa.marjanov@outlook.com>
2023-08-19 07:15:02 -0500

    add conmtact page hero

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,9 +1,17 @@
+import { HeroServices } from "@/components"
+import ContactForm from "@/components/ContactForm"
 
 const Contact = () => {
 
     return (
-        <main>
-            <h3 className="text-teal-600 text-3xl md:text-6xl font-bold">Contact our Expert team!</h3> </main>
+        <main className="w-full flex items-center justify-center flex-col">
+            <div className="relative w-full h-screen">
+                <HeroServices src="/dentures.jpg" heading="Get in Touch" message="Fill Out Form Below" />
+            </div>
+            <ContactForm />
+            <h3 className="text-teal-600 text-3xl py-6 md:text-6xl font-bold">Contact our Expert team!</h3>
+        </main>
+
     )
 }
 
